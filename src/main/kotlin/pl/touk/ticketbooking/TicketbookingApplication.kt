e02fb1a05fd4fdea20edb4d650ab668b52b98f2b
Michał Chojnowski <mc394134@students.mimuw.edu.pl>
2019-05-05 14:43:31 +0200

    Spring Initializr template

---
+++ src/main/kotlin/pl/touk/ticketbooking/TicketbookingApplication.kt
@@ -0,0 +1,11 @@
+package pl.touk.ticketbooking
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TicketbookingApplication
+
+fun main(args: Array<String>) {
+	runApplication<TicketbookingApplication>(*args)
+}
